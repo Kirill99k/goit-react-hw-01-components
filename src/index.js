21389003bec2f3f7bb4961e14e97120f26a27d4e
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-05-02 19:52:00 +0300

    Adds a index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'constants/theme';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
