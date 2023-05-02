f26b75502ed606f4080cd5d94c31b106ead4eadb
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-05-02 19:51:13 +0300

    Adds a App and AppStyled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import PropTypes from 'prop-types';
+import { GlobalStyle } from "constants/GlobalStyles";
+import { Container } from "./AppStyled";
+import { Profile } from "./Profile";
+import { Statistics } from "./Statistics";
+import { FriendList } from "./FriendList";
+import { TransactionsHistory } from "./TransactionHistory";
+import user from "data/user.json";
+import friends from 'data/friends.json'
+import transactions from 'data/transactions.json'
+import {data} from 'data/data'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Profile username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionsHistory transactions={transactions} />
+    </Container>
   );
 };
+
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.object.isRequired,
+}
